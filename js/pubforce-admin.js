4ac72c1c8895fb223bcc66c5245a1adf38d8e8ff
khoda <omidmava@gmail.com>
2013-04-24 22:00:27 +0200

    inital commit

---
+++ js/pubforce-admin.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function($)
+{	
+$(".tfdate").datepicker({
+    dateFormat: 'yy-mm-dd',
+    showOn: 'button',
+    buttonImage: 'https://khoda.se/wp/wp-content/themes/rbc/images/icon-datepicker.png',
+    buttonImageOnly: true,
+    numberOfMonths: 1
+
+    });
+});
